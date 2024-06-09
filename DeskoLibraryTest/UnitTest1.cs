f31524d98ed8ceff54d8d7280aa4f4c7268c574e
Fabio Caldas <fabio.caldas@gmail.com>
2024-06-08 21:22:59 -0300

    Added two new projects: `DeskoLibrary` and `DeskoLibraryTest`, both have been created, targeting .NET 8.0 framework.
    #5

---
+++ DeskoLibraryTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace DeskoLibraryTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
